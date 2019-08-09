6b643d9307a4f75d68818cd0cb41723b3ccc0cbb
woodpenker <woodpenker@163.com>
2019-08-09 13:10:11 +0800

    fix: dir error

--- init.sh
+++ init.sh
@@ -5,30 +5,41 @@ else
     echo "tmux not install"
     exit 1
 fi
-cd $HOME/
-mkdir -p $HOME/.tmux/plugins
-cd $HOME/.tmux/plugins
-git clone https://github.com/tmux-plugins/tmux-resurrect.git
-git clone https://github.com/tmux-plugins/tmux-continuum.git
-git clone https://github.com/tmux-plugins/tpm
-if which vim;then
-if [ ! -e $HOME/.vim/bundle ];then
-    mkdir -p $HOME/.vim/bundle
+current_dir="$(pwd)"
+if [ ! -e "$HOME/.tmux/plugins" ];then
+    mkdir -p "$HOME/.tmux/plugins"
 fi
-cd $HOME/.vim/bundle
-git clone git://github.com/tpope/vim-obsession.git
-vim -u NONE -c "helptags vim-obsession/doc" -c q
+cd "$HOME/.tmux/plugins"
+if [ ! -e "$HOME/.tmux/plugins/tmux-resurrect" ];then
+    git clone https://github.com/tmux-plugins/tmux-resurrect.git
+fi
+if [ ! -e "$HOME/.tmux/plugins/tmux-continuum" ];then
+    git clone https://github.com/tmux-plugins/tmux-continuum.git
+fi
+if [ ! -e "$HOME/.tmux/plugins/tpm" ];then
+    git clone https://github.com/tmux-plugins/tpm
+fi
+if which vim;then
+    if [ ! -e "$HOME/.vim/bundle" ];then
+        mkdir -p "$HOME/.vim/bundle"
+    fi
+    cd $HOME/.vim/bundle
+    if [ ! -e "$HOME/.vim/bundle/vim-obsession" ];then
+        git clone git://github.com/tpope/vim-obsession.git
+        vim -u NONE -c "helptags vim-obsession/doc" -c q
+    fi
 else
     echo "vim not install"
 fi
+cd "${current_dir}"
 cp tmux.conf $HOME/.tmux.conf
 cp up.sh $HOME/up.sh
 chmod +x $HOME/up.sh
 cp zshrc $HOME/.zshrc
 if which zhs;then
     chsh -s $(which zsh)
+    source $HOME/.zshrc
 else
     echo "zsh not install"
     exit 1
 fi
-source $HOME/.zshrc
